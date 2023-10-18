3403758abd4f05e60bb9cb48646519f55f990e2e
Erik Diers <dark.erik02@gmail.com>
2023-10-18 12:13:26 +0200

    Added a simple Data preprocessing and a Dataloader.
    Moreover added the UTK Face Dataset

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import DataLoader
+
+PREPROCESSED_DATASET_FOLDER = "data\\preprocessed"
+UTK_FACE_FOLDER = "data\\UTKFace"
+UTK_DATASET_NAME = "utk"
+
+PREPROCESS_DATA = True
+
 
 def main():
-    pass
+    if PREPROCESS_DATA:
+        DataLoader.preprocess_UTK_images(UTK_FACE_FOLDER, PREPROCESSED_DATASET_FOLDER, UTK_DATASET_NAME)
 
+    dataset = DataLoader.load_dataset_from_preprocessed(PREPROCESSED_DATASET_FOLDER, UTK_DATASET_NAME)
 
 if __name__ == '__main__':
     main()
